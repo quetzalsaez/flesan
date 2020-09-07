6e96cf725288e38a22a0a86983cd205bfc854ab5
quetzalsaez <hola@quetzal.cl>
2020-09-07 11:55:36 -0300

    server

---
+++ build/precache-manifest.2b142e40728d14dee338787b3be57c91.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "446e66855d9350a59cfd475d86411044",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ba48315f515ff442ffa",
+    "url": "/static/css/2.40ce3aac.chunk.css"
+  },
+  {
+    "revision": "7005f55d23e64eb8221c",
+    "url": "/static/css/main.a3d4bcc1.chunk.css"
+  },
+  {
+    "revision": "9ba48315f515ff442ffa",
+    "url": "/static/js/2.eee0cc79.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.eee0cc79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7005f55d23e64eb8221c",
+    "url": "/static/js/main.f6c6bf34.chunk.js"
+  },
+  {
+    "revision": "0ab3ad3933580a19c49d",
+    "url": "/static/js/runtime-main.e3c25d33.js"
+  },
+  {
+    "revision": "d211b734e7dd1d6f7d0bbcfd9b934171",
+    "url": "/static/media/construccion.d211b734.jpg"
+  },
+  {
+    "revision": "2f0d692377e734bca8a9137434068587",
+    "url": "/static/media/logo.2f0d6923.png"
+  }
+]);
