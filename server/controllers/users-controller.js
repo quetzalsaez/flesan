a7d9331ee8cba1f3ae8af52bffdef2c4d08cd646
quetzalsaez <hola@quetzal.cl>
2020-09-08 17:27:15 -0300

    express test

---
+++ server/controllers/users-controller.js
@@ -0,0 +1,6 @@
+// Import json with list of users
+const users = require('./../data/users.json')// Create controller for GET request to '/users/all'
+exports.usersGetAll = async (req, res) => {
+  // res.send('There will be dragons, not posts.')
+  res.json(users)
+}
